2d202a47895a3e3f8fde03df95501ccbc935e698
srinath <srinath.azmeera1995@gmail.com>
2018-09-29 17:13:07 +0530

    changed init to get app

--- electron/__init__.py
+++ electron/__init__.py
@@ -5,25 +5,22 @@ import json
 import strings
 import os
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+app.config.from_pyfile('config.py', silent=True)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
 
-    """" entry point for webapp """
-    @app.route("/")
-    def main():
-        return render_template('index.html', topics=strings.library, my=strings.personal_data)
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
 
-    return app
+"""" entry point for webapp """
+@app.route("/")
+def main():
+    return render_template('index.html', topics=strings.library, my=strings.personal_data)
+
+
+if __name__ == '__main__':
+    app.run()
