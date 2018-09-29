c2d39876ae678dd5d447e2abb91d54848326b62c
srinath <srinath.azmeera1995@gmail.com>
2018-09-29 13:28:14 +0530

    basic web app for home page

---
+++ electron/strings.py
@@ -0,0 +1,158 @@
+
+"""" contains all string data for populating various fields on the UI """
+
+# HOME PAGE
+
+personal_data = {
+					'quote'			:		"All is well",
+					'about'			:		"""
+											   Hi, This is Srinath. I love Technology, Books, Nature and much more.
+											   I grew up in a village near Bhupalpally, Warangal, Telangana. I have made this web page to
+											   access some of the eduactional resources and for experimental purposes. Feel free to email
+											   me for any queries, opinions or suggestions.
+											""",
+	   "research_interests"			:		"""
+	   											I am still exploring. Some of the topics of my interest lies in Computer Vision, Learning
+	   											Agents, Operating Systems and Compilers.
+	   										""",
+	   "education_bachelors"		: 		"Bachelors in CSE, IIT Bombay",
+	   	   "education_plus2"		: 		"+2, Narayana Junior College, Hyderabad",
+	   	   "education_tenth"		:		"SSC, SPR Schools of Excellence, Hanamkonda",
+
+	   	    "contact_email"			: 		"srinath.azmeera1995@gmail.com",
+	   	   "contact_mobile"			: 		"9967180296"
+}
+
+
+library = [
+	{
+		"topic": "Computer Science Theory",
+		"courses": [{
+				"title"	: "Logic In Computer Science",
+				"author": "Huth & Ryan",
+				"source": "LogicInComputerScience_Huth&Ryan.pdf"
+			},
+			{
+				"title"	: "Automata & Computability",
+				"author": "Dexter C. Kozen",
+				"source": "DexterKozen.pdf"
+			},
+			{
+				"title"	: "Introduction to Automata Theory",
+				"author": "Hopcraft, Motwani & Ullman",
+				"source": "HopcroftMotwaniUllman2ndEdition.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Programming",
+		"courses": [{
+				"title"	: "Data Structures & Algorithms",
+				"author": "T.H Cormen",
+				"source": "DataStructures&Algorithms_Cormen.pdf"
+			},
+			{
+				"title"	: "Algorithm Design",
+				"author": "Tardos",
+				"source": "AlgorithmDesign_ EvaTardos.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Data Analytics & Statistics",
+		"courses": [{
+				"title"	: "Pattern Classification",
+				"author": "Duda",
+				"source": "PatternClassification_Duda.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Networks & Security",
+		"courses": [{
+				"title"	: "Computer Networks A System Approach",
+				"author": "",
+				"source": "ComputerNetworksSystemsApproach.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Computer Vision",
+		"courses": [{
+				"title"	: "Digital Image Processing",
+				"author": "Gonzalez & Woods",
+				"source": "DigitalImageProcessing_Gonzalez&Woods.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Databases",
+		"courses": [{
+				"title"	: "Databases & Information Systems",
+				"author": "Sudarshan",
+				"source": "Databases&InformationSystems.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Operating Systems",
+		"courses": [{
+				"title"	: "Operating System Concepts",
+				"author": "",
+				"source": "OperatingSystemConcepts.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Artificial Intelligence & ML",
+		"courses": [{
+				"title"	: "Artificial Intelligence A Modern Approach",
+				"author": "",
+				"source": "AIMA.pdf"
+			},
+			{
+				"title"	: "Foundations of Intelligent and Learning Agents",
+				"author": "Sutton Barto",
+				"source": "Sutton&Barto.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Computer Architecture",
+		"courses": [{
+				"title"	: "Digital Logic Design",
+				"author": "Morris Mano",
+				"source": "DigitalDesign_MorrisMano.pdf"
+			},
+			{
+				"title"	: "Computer Organisation & Design",
+				"author": "",
+				"source": "ComputerOrganisation&Design.pdf"
+			}
+		]
+	},
+
+	{
+		"topic": "Compilers",
+		"courses": [{
+				"title"	: "Dragonbook-Compilers",
+				"author": "Ullman",
+				"source": "ALSUdragonbook.pdf"
+			}
+		]
+	}
+
+
+]
+
+
+
+# END HOME PAGE
